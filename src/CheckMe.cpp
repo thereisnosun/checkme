fcdd74ee83d847a2418bc3e76b04a7b198252fd3
Teodor Moroz <teomoroz@gmail.com>
2016-12-01 19:41:52 +0200

    added test code for finding lines

--- src/CheckMe.cpp
+++ src/CheckMe.cpp
@@ -4,26 +4,13 @@
 #include <Windows.h>
 #include <iostream>
 #include <set>
+#include <cmath>
 #include "utils.h"
 #include "NoiseReducer.h"
 using namespace cv;
 
 const int THRESHOLD = 128;
-
-void FindLines(const Mat &adaptiveImage)
-{
-    std::vector<Vec4i> vLines;
-    HoughLinesP(adaptiveImage, vLines, CV_PI / 180, 50, 50, 10);
-    Mat cdst;
-    cvtColor(adaptiveImage, cdst, CV_GRAY2BGR);
-    for (size_t i = 0; i < vLines.size(); i++)
-    {
-        Vec4i l = vLines[i];
-        line(cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, CV_AA);
-    }
-    imshow("detected lines", cdst);
-    waitKey();
-}
+const int FLUFF = 30;
 
 void SaveImage(const Mat &image, const std::string &sImageName)
 {
@@ -34,6 +21,54 @@ void SaveImage(const Mat &image, const std::string &sImageName)
     }
 #endif
 }
+
+void FindLines(const Mat &adaptiveImage)
+{
+    std::vector<Vec4i> vLines;
+    vLines.reserve(adaptiveImage.rows*adaptiveImage.cols);
+    HoughLinesP(adaptiveImage, vLines, 1, CV_PI / 180, 128, 30, 10);
+    Mat cdst(adaptiveImage.size(), CV_8UC1);
+    cdst = Scalar(255);
+    //Mat cdst;
+    //cvtColor(adaptiveImage, cdst, CV_GRAY2BGR);
+
+    Vec4i longestLine;
+    int iHeight = adaptiveImage.rows;
+    std::cout << "Line detection is done. Total number of lines is - " << vLines.size() << "\n";
+    //first take care of vertical lines
+    for (size_t i = 0; i < vLines.size(); i++)
+    {
+        Vec4i curLine = vLines[i];
+        
+        int x1 = curLine[0];
+        int y1 = curLine[1];
+
+        int x2 = curLine[2];
+        int y2 = curLine[3];
+      
+        if (y1 < FLUFF && iHeight - y2 < FLUFF ||
+            iHeight - y1 < FLUFF && y2 < FLUFF)
+        {
+            longestLine = curLine;
+            line(cdst, Point(x1, y1), Point(x2, y2), Scalar(0, 0, 255), 3, CV_AA);
+            std::cout << "Drawing the line... Iteration number " << i << ". Point 1 - ("
+                << x1 << ", " << y1 << "). Point 2 - (" << x2 << ", " << y2 << ").\n";
+        }
+       
+        //line(cdst, Point(curLine[0], curLine[1]), Point(curLine[2], curLine[3]), Scalar(0, 0, 255), 3, CV_AA);
+    }
+    //TODO: handle horizontal lines
+
+    SaveImage(adaptiveImage, "lines.jpg");
+    namedWindow("Rock", WINDOW_NORMAL);
+    resizeWindow("Rock", adaptiveImage.cols/5, adaptiveImage.rows/5);
+    imshow("Rock", cdst);
+    waitKey();
+     
+}
+
+//TODO:
+//Write test in parallel, practice to use cppunit, gmock
 
 int main(int argc, char *argv[])
 {
@@ -61,7 +96,7 @@ int main(int argc, char *argv[])
         std::cout << "collumns - " << normalizedImage.cols << " rows - " << normalizedImage.rows << "\n";
 
         Mat binaryImage;
-        threshold(normalizedImage, binaryImage, 128, 255, CV_THRESH_BINARY); //this one really nessecary for clear picture
+        threshold(normalizedImage, binaryImage, 127, 255, CV_THRESH_BINARY); //this one really nessecary for clear picture
         std::string sBinaryImage = AddSuffix(sImagePath, "binary");
         SaveImage(binaryImage, sBinaryImage);
 
@@ -69,9 +104,11 @@ int main(int argc, char *argv[])
         adaptiveThreshold(binaryImage, adaptiveImage, 255, ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY, 11, 1);
         std::string sAdaptImage = AddSuffix(sImagePath, "adaptive");
         SaveImage(adaptiveImage, sAdaptImage);
+        
+        FindLines(adaptiveImage);
+
         //NoiseReducer reducer;
         //reducer.RemoveNoise(adaptiveImage, MINIMAL_PIECE_SIZE);
-        ////FindLines(adaptiveImage);
         //std::string sClearImage = AddSuffix(sImagePath, "clear");
         //SaveImage(adaptiveImage, sClearImage);
     }
